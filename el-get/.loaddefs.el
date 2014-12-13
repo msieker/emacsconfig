f18c4aa392127d0c48313ea97c55c0385fb9ac2d
Matt Sieker <matt@siekernet.com>
2014-12-13 12:45:58 -0600

    Initial commit

---
+++ el-get/.loaddefs.el
@@ -0,0 +1,199 @@
+;;; .loaddefs.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (arx-or arx-and define-arx) "ample-regexps" "ample-regexps/ample-regexps.el"
+;;;;;;  (21639 54389))
+;;; Generated autoloads from ample-regexps/ample-regexps.el
+
+(autoload 'define-arx "ample-regexps" "\
+Generate a custom rx-like macro under name MACRO.
+
+See `rx' for how the generated macro can be invoked.
+
+FORM-DEFS is a list of custom s-exp definitions to create whose
+elements have the form (SYM DEF), where DEF is one of
+the following:
+
+- \"LITERAL\" -- create a matcher to match a string literally
+
+- (regexp \"LITERAL\") -- create a match given a regexp
+
+- SYMBOL -- create an alias for a symbol either defined earlier
+  on the list or provided by `rx'
+
+- (SUBFORM ...) -- create an alias for an application of s-exp
+  subform either defined earlier on the list or provided by `rx'
+
+- (:func #'FORM-FUNC ...) -- create an s-exp definition
+
+The most interesting here is the last variant.  When a
+corresponding rx form will be encountered, FORM-FUNC will be
+called with all elements of that form as arguments (with the
+first one being the form symbol itself).  FORM-FUNC must then
+return a valid s-exp or a properly grouped plain regexp.
+
+Another keywords that are recognized in the plist are:
+- :min-args -- minimum number of arguments for that form (default nil)
+- :max-args -- minimum number of arguments for that form (default nil)
+- :predicate -- if given, all rx form arguments must satisfy it
+
+\(fn MACRO FORM-DEFS)" nil (quote macro))
+
+(autoload 'arx-and "ample-regexps" "\
+Generate an expression to match a sequence of FORMS.
+
+\(fn FORMS)" nil nil)
+
+(autoload 'arx-or "ample-regexps" "\
+Generate an expression to match one of FORMS.
+
+\(fn FORMS)" nil nil)
+
+;;;***
+
+;;;### (autoloads (el-get el-get-self-checksum el-get-checksum el-get-make-recipes
+;;;;;;  el-get-cd el-get-reinstall el-get-remove el-get-self-update
+;;;;;;  el-get-update-packages-of-type el-get-update-all el-get-update
+;;;;;;  el-get-install el-get-version) "el-get" "el-get/el-get.el"
+;;;;;;  (21639 54140))
+;;; Generated autoloads from el-get/el-get.el
+
+(autoload 'el-get-version "el-get" "\
+Message the current el-get version
+
+\(fn)" t nil)
+
+(autoload 'el-get-install "el-get" "\
+Cause the named PACKAGE to be installed after all of its
+dependencies (if any).
+
+PACKAGE may be either a string or the corresponding symbol.
+
+\(fn PACKAGE)" t nil)
+
+(autoload 'el-get-update "el-get" "\
+Update PACKAGE.
+
+\(fn PACKAGE)" t nil)
+
+(autoload 'el-get-update-all "el-get" "\
+Performs update of all installed packages.
+
+\(fn &optional NO-PROMPT)" t nil)
+
+(autoload 'el-get-update-packages-of-type "el-get" "\
+Update all installed packages of type TYPE.
+
+\(fn TYPE)" t nil)
+
+(autoload 'el-get-self-update "el-get" "\
+Update el-get itself.  The standard recipe takes care of reloading the code.
+
+\(fn)" t nil)
+
+(autoload 'el-get-remove "el-get" "\
+Remove any PACKAGE that is know to be installed or required.
+
+\(fn PACKAGE &optional PACKAGE-STATUS-ALIST)" t nil)
+
+(autoload 'el-get-reinstall "el-get" "\
+Remove PACKAGE and then install it again.
+
+\(fn PACKAGE)" t nil)
+
+(autoload 'el-get-cd "el-get" "\
+Open dired in the package directory.
+
+\(fn PACKAGE)" t nil)
+
+(autoload 'el-get-make-recipes "el-get" "\
+Loop over `el-get-sources' and write a recipe file for each
+entry which is not a symbol and is not already a known recipe.
+
+\(fn &optional DIR)" t nil)
+
+(autoload 'el-get-checksum "el-get" "\
+Compute the checksum of the given package, and put it in the kill-ring
+
+\(fn PACKAGE &optional PACKAGE-STATUS-ALIST)" t nil)
+
+(autoload 'el-get-self-checksum "el-get" "\
+Compute the checksum of the running version of el-get itself.
+
+Also put the checksum in the kill-ring.
+
+\(fn)" t nil)
+
+(autoload 'el-get "el-get" "\
+Ensure that packages have been downloaded once and init them as needed.
+
+This will not update the sources by using `apt-get install' or
+`git pull', but it will ensure that:
+
+* the packages have been installed
+* load-path is set so their elisp files can be found
+* Info-directory-list is set so their info files can be found
+* Autoloads have been prepared and evaluated for each package
+* Any post-installation setup (e.g. `(require 'feature)') happens
+
+When SYNC is nil (the default), all installations run
+concurrently, in the background.
+
+When SYNC is 'sync, each package will be installed synchronously,
+and any error will stop it all.
+
+Please note that the `el-get-init' part of `el-get' is always
+done synchronously. There's `byte-compile' support though, and
+the packages you use are welcome to use `autoload' too.
+
+PACKAGES is expected to be a list of packages you want to install
+or init.  When PACKAGES is omited (the default), the list of
+already installed packages is considered.
+
+\(fn &optional SYNC &rest PACKAGES)" nil nil)
+
+;;;***
+
+;;;### (autoloads (el-get-list-packages) "el-get-list-packages" "el-get/el-get-list-packages.el"
+;;;;;;  (21639 54140))
+;;; Generated autoloads from el-get/el-get-list-packages.el
+
+(autoload 'el-get-list-packages "el-get-list-packages" "\
+Display a list of packages.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads (lua-mode) "lua-mode" "lua-mode/lua-mode.el" (21639
+;;;;;;  54400))
+;;; Generated autoloads from lua-mode/lua-mode.el
+
+(autoload 'lua-mode "lua-mode" "\
+Major mode for editing Lua code.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
+;;;***
+
+;;;### (autoloads nil nil ("el-get/el-get-autoloading.el" "el-get/el-get-build.el"
+;;;;;;  "el-get/el-get-byte-compile.el" "el-get/el-get-core.el" "el-get/el-get-custom.el"
+;;;;;;  "el-get/el-get-dependencies.el" "el-get/el-get-install.el"
+;;;;;;  "el-get/el-get-methods.el" "el-get/el-get-notify.el" "el-get/el-get-recipes.el"
+;;;;;;  "el-get/el-get-status.el") (21639 54413 728218))
+
+;;;***
+
+(provide '.loaddefs)
+;; Local Variables:
+;; version-control: never
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; .loaddefs.el ends here
---
+++ init.el
@@ -0,0 +1,51 @@
+;;(require 'color-theme)
+;;(setq color-theme-is-global t)
+;;(color-theme-initialize)
+;;(color-theme-gray30)
+
+
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
+
+(load "~/.emacs.d/web-mode.el")
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 4)
+  (setq web-mode-css-indent-offset 4)
+  (setq web-mode-code-indent-offset 4)
+  )
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-archives
+   (quote
+    (("melpa" . "http://melpa.org/packages/")
+     ("marmalade" . "http://marmalade-repo.org/packages/")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
